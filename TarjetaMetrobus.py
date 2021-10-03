f9d9675295ff17c44c1a854bac79ba6a71ed7ff9
Miguel Mauricio Delgado Fernández <73376209+mdelgadof1300@users.noreply.github.com>
2021-10-03 01:46:06 -0500

    Update TarjetaMetrobus.py

--- TarjetaMetrobus.py
+++ TarjetaMetrobus.py
@@ -2,7 +2,44 @@ class TarjetaMetrobus():
     def __init__(self):
         self.__saldo = 0
 
-    def recargar(self):
+    def recargar(self, monto):
+        saldotemp = self.__saldo+monto
+        if all([saldotemp>1, saldotemp<=120]):
+            self.__saldo = saldotemp
+            return True
+        return False
         
-
     def cobrar(self):
+        saldotemp = self.__saldo-6
+        if saldotemp>=0:
+            self.__saldo=self.__saldo-6
+            return True
+        return False
+    def getSaldo(self):
+        return self.__saldo
+def menu():
+    print("1. Recargar")
+    print("2. Pasar al metrobús")
+    print("3. Consultar saldo")
+    print("4. Salir")
+
+tarjeta = TarjetaMetrobus()
+while True:
+    menu()
+    opcion = int(input("Opción: "))
+    if opcion == 1:
+        saldo = int(input("Saldo: "))
+        if tarjeta.recargar(saldo):
+            print("¡Recarga exitosa!")
+        else:
+            print("¡Recarga fallida!")
+    elif opcion == 2:
+        if tarjeta.cobrar():
+            print("Pase usted")
+        else:
+            print("Saldo insuficiente")
+    elif opcion == 3:
+        print("Su saldo es: $" + str(tarjeta.getSaldo()))
+    elif opcion == 4:
+        print("Gracias por su elección")
+        exit(1)
